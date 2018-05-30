b353bd7202d97f5740048df815b89edadb5e3e0a
Demeco <dbell1911@gmail.com>
2018-05-29 20:30:34 -0400

    server updated

--- app.js
+++ app.js
@@ -23,21 +23,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
-res.send('Please use routes for application access');
+    res.send('Please use routes for application access');
+});
+
+//shows all data in the db
+
+app.get('/api/allData', (req, res) => {
+    mongoose.connect(DB_URL, function (err, db) {
+        if (err) throw err;
+        var collection = db.collection('patientInformation');
+        db.collection('patientInformation').find({}).toArray(function (err, result) {
+            if (err) throw err;
+            return res.status(200)
+            .json(result);
+            db.close();
+        });
+    });
+
 });
 
 app.post('/api/add', (req, res) => {
-     // Insert JSON straight into MongoDB
-     mongoose.connect(DB_URL, function(err, db) {
-        if(err) { throw err;  }
+    // Insert JSON straight into MongoDB
+    mongoose.connect(DB_URL, function (err, db) {
+        if (err) { throw err; }
         var collection = db.collection('patientInformation');
-        var patientInfo = {  Name: req.body.Name,  Email:req.body.Email, Phone: req.body.Phone, Address:req.body.Address, Medication:req.body.Medication, Diagnosis:req.body.Diagnosis, AdditionalInfo:req.body.AdditionalInfo };
-        collection.insert(patientInfo, function(err, result) {
-        if(err) { throw err; }
-          db.close();
-          res.send('Information Updated');   
-      });
-      });
+        var patientInfo = { Name: req.body.Name, Email: req.body.Email, Phone: req.body.Phone, Address: req.body.Address, Medication: req.body.Medication, Diagnosis: req.body.Diagnosis, AdditionalInfo: req.body.AdditionalInfo };
+        collection.insert(patientInfo, function (err, result) {
+            if (err) { throw err; }
+            db.close();
+            res.send('Information Updated');
+        });
     });
+});
 
 app.listen(3000, () => console.log('server is running'));
