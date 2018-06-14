8612492b982f6b838b6da363decd27143927f551
Demeco <dbell1911@gmail.com>
2018-06-14 10:44:35 -0400

    update user model

--- models/user.js
+++ models/user.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    username: String,
-    password: String,
+    username: {type: String, required: true},
+    password: {type: String, required: true},
 });
 
 module.exports = mongoose.model('User', UserSchema);
