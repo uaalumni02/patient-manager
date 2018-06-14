9edea6e15ee413b0af41230f054cb13af1192885
Demeco <dbell1911@gmail.com>
2018-06-14 12:26:11 -0400

    added password hash

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,65 +1,44 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
 const router = express.Router();
 
 //request model
-var PatientInformation = require('../models/user');
+var User = require('../models/user');
 
-//shows all data
 
-router.get('/', (req, res) => {
-    PatientInformation.find()
-        .exec()
-        .then(docs => {
-            res.status(200).json(docs);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-});
-
-//add info to db
+//create user
 router.post('/', (req, res, next) => {
-    var patientInformation = new PatientInformation({
-        _id: new mongoose.Types.ObjectId(),
-        username: req.body.username,
-        password: req.body.password,
-        
-    });
-    patientInformation
-        .save()
-        .then(result => {
-        })
-        .catch(err => console.log(err));
-    res.status(201).json({
-        message: 'added to database',
-        updatedPatient: patientInformation
-    });
-});
-
-
-
-//remove docs from the db
-router.delete('/:id', (req, res) => {
-    var id = req.params.id;
-    PatientInformation.findOneAndDelete({ '_id': id })
-        .exec()
-        .then(result => {
-            res.status(200).json(result);
-            
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+            return res.status(500).json({
                 error: err
             });
-        });
-});
+        } else {
+            var user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                username: req.body.username,
+                password: hash
+            });
+            user
+                .save()
+                .then(result => {
+                    console.log(result)
+                    res.status(201).json({
+                        message: 'User Created',
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({
+                        error: err
 
+                    });
+                });
+        }
+    });
+});
 
 
 
