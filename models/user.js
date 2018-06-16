eb41a79920f78847a2b4b316332e2e5b86c41a67
Demeco <dbell1911@gmail.com>
2018-06-15 20:28:29 -0400

    updated user model

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    username: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
 });
 
