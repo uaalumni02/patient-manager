6b7001f5e0938223b34c56248447d9a0713270af
Demeco <dbell1911@gmail.com>
2018-06-15 20:17:14 -0400

    updated user hash

--- routes/user.route.js
+++ routes/user.route.js
@@ -10,34 +10,47 @@ var User = require('../models/user');
 
 //create user
 router.post('/', (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            return res.status(500).json({
-                error: err
-            });
-        } else {
-            var user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                username: req.body.username,
-                password: hash
-            });
-            user
-                .save()
-                .then(result => {
-                    console.log(result)
-                    res.status(201).json({
-                        message: 'User Created',
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({
-                        error: err
-
-                    });
+    User.find({ user: req.body.user })
+        .exec()
+        .then(user => {
+            if (user.length >=1 ) {
+                return res.status(409).json({
+                    message: 'user name exist'
                 });
-        }
-    });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+
+                    }
+                    var user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        username: req.body.username,
+                        password: hash
+                    });
+                    user
+                        .save()
+                        .then(result => {
+                            console.log(result)
+                            res.status(201).json({
+                                message: 'User Created',
+                            });
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({
+                                error: err
+
+                            });
+                        });
+                })
+
+            }
+        })
+
 });
 
 
