6fa4e177e20b1cdeb965382ea172b2a10a9a037e
Demeco <dbell1911@gmail.com>
2018-06-25 19:37:34 -0400

    updated controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,60 @@
-var PatientInformation = require('../models/user');
 
 var express = require("express");
-
 var router = express.Router();
-
 var mongoose = require('mongoose');
 
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 
+//import model
+var User = require('../models/user');
+
+
+
+//create user
+router.createUser = ('/', (req, res, next) => {
+    User.find({ username: req.body.username })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'user name exist'
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+
+                    }
+                    var user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        username: req.body.username,
+                        password: hash
+                    });
+                    user
+                        .save()
+                        .then(result => {
+                            console.log(result)
+                            res.status(201).json({
+                                message: 'User Created',
+                            });
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({
+                                error: err
+
+                            });
+                        });
+                })
+
+            }
+        })
+
+});
 
 
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -8,51 +8,12 @@ const router = express.Router();
 //request model
 var User = require('../models/user');
 
+// import controller
+var userController = require('../controllers/user');
+
 
 //create user
-router.post('/', (req, res, next) => {
-    User.find({ username: req.body.username })
-        .exec()
-        .then(user => {
-            if (user.length >= 1) {
-                return res.status(409).json({
-                    message: 'user name exist'
-                });
-            } else {
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if (err) {
-                        return res.status(500).json({
-                            error: err
-                        });
-                    } else {
-
-                    }
-                    var user = new User({
-                        _id: new mongoose.Types.ObjectId(),
-                        username: req.body.username,
-                        password: hash
-                    });
-                    user
-                        .save()
-                        .then(result => {
-                            console.log(result)
-                            res.status(201).json({
-                                message: 'User Created',
-                            });
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            res.status(500).json({
-                                error: err
-
-                            });
-                        });
-                })
-
-            }
-        })
-
-});
+router.post('/', userController.createUser);
 
 router.post('/login', (req, res, next) => {
     User.find({ user: req.body.user })
