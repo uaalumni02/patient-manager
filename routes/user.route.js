1c696e2a1595890577be7d5103e054a3b08af86f
Demeco <dbell1911@gmail.com>
2018-07-23 21:18:01 -0400

    updated routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,22 +3,18 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import checkAuth from '../middleware/check-auth';
 
-const router = express.Router();
-
 //import model
 import User from '../models/user';
-
 // import controller
 import userController from '../controllers/user';
 
+const router = express.Router();
+
 //create user
 router.post('/', userController.createUser);
-
 //user login
 router.post('/login', userController.logIn);
-
 //delete user
 router.delete('/:id', userController.removeUser);
 
-
-module.exports = router;
+export default router;
